177f7302f6b4ac26a3fee2a00ae01dd4cae6a140
Pavo ┼áimi─ç <psimic@vub.hr>
2024-06-23 17:48:18 +0200

    Dodana klasa DataFetcher za asinkrono dohvaćanje podataka s podrškom za višedretvenost

---
+++ DataFetcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MedicinskaOrdinacija {
+    public class DataFetcher {
+        private readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+        public async Task<List<Pacijent>> FetchPacijentiAsync() {
+
+           await semaphoreSlim.WaitAsync();
+            try {
+                using (var context = new OrdinacijaDB()) {
+                    return await context.Pacijenti.ToListAsync();
+                }
+            }
+            finally {
+                semaphoreSlim.Release();
+            }
+        }
+        public async Task<List<Doktor>> FetchDoktoriAsync() {
+            await semaphoreSlim.WaitAsync();
+            try {
+                using (var context = new OrdinacijaDB()) {
+                    return await context.Doktori.ToListAsync();
+                }
+            }
+            finally {
+                semaphoreSlim.Release();
+            }
+        }
+    }
+}
